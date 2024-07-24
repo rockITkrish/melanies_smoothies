fd155d9796c34792885a46e09fc526932cb9b1c8
rockITkrish <125395328+rockITkrish@users.noreply.github.com>
2024-07-24 13:07:52 -0400

    Update streamlit_app.py
    Tab/Indent fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ if ingredients_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+	    ingredients_string += fruit_chosen + ' '
+	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     #st.write(ingredients_string);
     
@@ -45,6 +45,6 @@ if ingredients_list:
     time_to_insert = st.button("Submit Order")
     
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-	
-        st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
+	    session.sql(my_insert_stmt).collect()
+	    
+	    st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
